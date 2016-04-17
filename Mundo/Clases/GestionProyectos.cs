841728a64661f6eff10c955572bcc04129285751
Felipe Clement <anyname15@hotmail.com>
2016-04-17 14:26:48 -0500

    Modelo Agregado

--- Mundo/Clases/GestionProyectos.cs
+++ Mundo/Clases/GestionProyectos.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Mundo
+namespace Mundo.Clases
 {
-    public class Class1
+    class GestionProyectos
     {
     }
 }
